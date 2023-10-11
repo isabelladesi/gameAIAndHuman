3ce47cc84437e29554c5f84499be7614b04e25e9
idesousa <idesousa@umich.edu>
2023-10-10 22:52:26 -0400

    player_tests updated

--- Player_tests.cpp
+++ Player_tests.cpp
@@ -86,25 +86,105 @@ TEST(test_player_play_card_NORMAL) {
 }
 
 
-// TEST(test_player_play_card_RBOWER) {
-//   // Bob's hand
-//   Player * bob = Player_factory("Bob", "Simple");
-//   bob->add_card(Card(ACE, SPADES));
-//   bob->add_card(Card(JACK, SPADES));
-//   bob->add_card(Card(QUEEN, SPADES));
-//   bob->add_card(Card(KING, SPADES));
-//   bob->add_card(Card(ACE, SPADES));
+TEST(test_player_play_card_RBOWER) {
+  // Bob's hand
+  Player * bob = Player_factory("Bob", "Simple");
+  bob->add_card(Card(ACE, SPADES));
+  bob->add_card(Card(JACK, SPADES));
+  bob->add_card(Card(QUEEN, SPADES));
+  bob->add_card(Card(KING, SPADES));
+  bob->add_card(Card(ACE, SPADES)); //ALL TRUMP CARDS
 
-//   // Bob plays a card
-//   Card nine_spades(JACK, SPADES);
-//   Card card_played = bob->play_card(
-//     nine_spades,  // Nine of Spades is led
-//     SPADES    // Trump suit
-//   ); 
+  // Bob plays a card
+  Card nine_spades(JACK, SPADES);
+  Card card_played = bob->play_card(
+    nine_spades,  // Nine of Spades is led
+    SPADES    // Trump suit
+  ); 
 
-//   // Verify the card Bob played
-//   ASSERT_EQUAL(card_played, Card(JACK, SPADES));
-//   delete bob;
-// }
+  // Verify the card Bob played
+  ASSERT_EQUAL(card_played, Card(JACK, SPADES));
+  delete bob;
+}
+
+TEST(test_player_make_trump_NORMAL_round1) {
+  // Bob's hand
+  Player * bob = Player_factory("Bob", "Simple");
+  bob->add_card(Card(ACE, HEARTS));
+  bob->add_card(Card(JACK, SPADES));
+  bob->add_card(Card(QUEEN, HEARTS));
+  bob->add_card(Card(KING, SPADES));
+  bob->add_card(Card(ACE, SPADES));
+
+  bool is_dealer = true;
+  Suit upCard_suit = Suit(DIAMONDS);
+  
+  Card jack_diamonds(JACK, DIAMONDS);
+  bool did_bob_make_trump = bob->make_trump(jack_diamonds, is_dealer,1,upCard_suit);
+  
+  // Verify the card Bob played
+  ASSERT_EQUAL(false, did_bob_make_trump);
+  delete bob;
+}
+
+TEST(test_player_make_trump_LB_round1) {
+  // Bob's hand
+  Player * bob = Player_factory("Bob", "Simple");
+  bob->add_card(Card(ACE, CLUBS));
+  bob->add_card(Card(NINE, DIAMONDS));
+  bob->add_card(Card(TEN, DIAMONDS));
+  bob->add_card(Card(JACK, HEARTS));
+  bob->add_card(Card(QUEEN, DIAMONDS));
+
+  bool is_dealer = false;
+  Suit upCard_suit = Suit(DIAMONDS); //trump suit is diamonds
+  
+  Card jack_diamonds(JACK, DIAMONDS); //upcard is king of diamonds
+  bool did_bob_make_trump = bob->make_trump(jack_diamonds, is_dealer,1,upCard_suit);
+  
+  // Verify the card Bob played
+  ASSERT_EQUAL(true, did_bob_make_trump);
+  delete bob;
+}
+
+TEST(test_player_make_trump_NORMAL_round2) {
+  // Bob's hand
+  Player * bob = Player_factory("Bob", "Simple");
+  bob->add_card(Card(KING, HEARTS));
+  bob->add_card(Card(ACE, HEARTS));
+  bob->add_card(Card(NINE, SPADES));
+  bob->add_card(Card(TEN, HEARTS));
+  bob->add_card(Card(QUEEN, DIAMONDS));
+
+  bool is_dealer = false;
+  Suit upCard_suit = Suit(DIAMONDS); //trump suit is diamonds
+  
+  Card jack_diamonds(JACK, DIAMONDS); //upcard is king of diamonds
+  bool did_bob_make_trump = bob->make_trump(jack_diamonds, is_dealer,2,upCard_suit);
+  
+  // Verify the card Bob played
+  ASSERT_EQUAL(true, did_bob_make_trump);
+  delete bob;
+}
+
+TEST(test_player_make_trump_dealer_round2) {
+  // Bob's hand
+  Player * bob = Player_factory("Bob", "Simple");
+  bob->add_card(Card(KING, SPADES));
+  bob->add_card(Card(EIGHT, HEARTS));
+  bob->add_card(Card(NINE, SPADES));
+  bob->add_card(Card(TEN, HEARTS));
+  bob->add_card(Card(QUEEN, DIAMONDS));
+
+  bool is_dealer = true;
+  Suit upCard_suit = Suit(DIAMONDS); //trump suit is diamonds
+  
+  Card jack_diamonds(JACK, DIAMONDS); //upcard is king of diamonds
+  bool did_bob_make_trump = bob->make_trump(jack_diamonds, is_dealer,2,upCard_suit);
+  
+  // Verify the card Bob played
+  ASSERT_EQUAL(true, did_bob_make_trump);
+  delete bob;
+}
 
 TEST_MAIN()

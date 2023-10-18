b15374c131e40ee96395691e0fd97beb8d5c17e6
Jordyn Vermut <jvermut@umich.edu>
2023-10-18 18:57:44 -0400

    no changes

--- euchre.cpp
+++ euchre.cpp
@@ -77,8 +77,12 @@ int main(int argc, char **argv) {
 class Game {
  public:
   Game(int POINTS_TO_WIN, string SHUFFLE, string NAME1, string TYPE1, string NAME2, 
-    string TYPE2, string NAME3, string TYPE3, string NAME4, string TYPE4);
-  void play();
+    string TYPE2, string NAME3, string TYPE3, string NAME4, string TYPE4){
+
+    }
+  void play(){
+    
+  }
 
  private:
   std::vector<Player*> players;

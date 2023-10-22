ac0f21a3b552639c7a0b9378a57aa1ecb6941a89
Jordyn Vermut <jvermut@umich.edu>
2023-10-22 13:16:38 -0400

    new tests

--- Card_tests.cpp
+++ Card_tests.cpp
@@ -49,4 +49,27 @@ TEST(test_card_is_left_bower) {
     ASSERT_EQUAL(false, a.is_left_bower(trump));
 }
 
+TEST(test_card_is_trump) {
+    Suit trump = Suit::HEARTS;
+    Card c(JACK, DIAMONDS);
+    ASSERT_EQUAL(false, c.is_trump(trump));
+    Card b(NINE, HEARTS);
+    ASSERT_EQUAL(true, b.is_trump(trump));
+}
+
+//not sure how to do this one
+// TEST(test_card_operator<<) {
+//     Card c(KING, SPADES);
+//     ASSERT_EQUAL("King of Spades", pack_input >> c);
+// }
+
+TEST(test_card_lower_value) {
+    Card b(NINE, CLUBS);
+    Card c(NINE, HEARTS);
+    ASSERT_EQUAL(false, b < c);
+    Card a(NINE, DIAMONDS);
+    Card d(TEN, DIAMONDS);
+    ASSERT_EQUAL(true, a < d);
+}
+
 TEST_MAIN()

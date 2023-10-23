08a8d93acf8a240a00959ecdf1c9c0a3957dd506
Jordyn Vermut <jvermut@umich.edu>
2023-10-23 14:07:22 -0400

    debugged

--- euchre.cpp
+++ euchre.cpp
@@ -99,7 +99,7 @@ class Game {
     pack = packPlay;
     POINTS_TO_WIN = points_to_win;
     SHUFFLE = shuffle;
-  }}
+  }
 
   //this is mainly copied so change this a lot
   void play(int POINTS_TO_WIN, vector<Player*> players, string SHUFFLE){
